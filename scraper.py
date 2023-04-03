b6c48f703c49cc38d573f68d81420c5f5159841f
nemo <ymillet.ping@gmail.com>
2023-04-03 12:11:45 +0200

    Update scraper to prevent loss from IP ban
    update csv in an except block in order not to have to scrap
    again the beginning of the batch in case of IP ban, or more
    generally of failure of a post scrap that terminates the scrap
    function

--- scraper.py
+++ scraper.py
@@ -1,12 +1,12 @@
 import instaloader
 import csv
 import pandas as pd
+from os.path import exists
 
 
 def download_instagram_post(shortcode):
     L = instaloader.Instaloader()
 
-    # .split récupère le shortcode du post et crée le post (récupéré grâce à son shortcode)
     SHORTCODE = shortcode
     post = instaloader.Post.from_shortcode(L.context, SHORTCODE)
 
@@ -16,12 +16,59 @@ def download_instagram_post(shortcode):
     L.download_post(post, target=target)
 
 
-def scrap(start_index, batch_size=100):
-    for sc in shortcodes[start_index : start_index + batch_size]:
-        download_instagram_post(sc)
+def scrap(links, batch_size=100):
+    for i in range(batch_size):
+        download_instagram_post(links[0])
+        links.remove(links[0])
 
 
+""" pour fabriquer le csv des liens pas encore scrapés
+# read the cleaned dataset
 data = pd.read_csv("cleaned.csv")
+# isolate link column
+shortcodes = data["link"]
+# convert it to list and remove doubles
+shortcodes = list(set(shortcodes.tolist()))
+# convert it back to dataframe
+shortcodes = pd.DataFrame({"link": shortcodes})
+# save it to csv
+shortcodes.to_csv("still_to_scrap.csv", index=False)
 
-shortcodes = list(set(data["link"].tolist()))
-scrap(400)
+shortcodes = pd.read_csv(("still_to_scrap.csv"))
+shortcodes = shortcodes["link"].tolist()
+print(shortcodes[:10])
+
+i = 0
+for link in shortcodes:
+    path = "media/" + str(link)
+    print(path)
+    if exists(path):
+        i += 1
+        print("on l'a déjà scrap donc ça dégage, c'est le {}ème".format(i))
+        shortcodes.remove(link)
+    else:
+        continue
+
+# convert to dataframe
+shortcodes = pd.DataFrame({"link": shortcodes})
+# save it to csv
+shortcodes.to_csv("still_to_scrap.csv", index=False)
+"""
+
+# read csv
+shortcodes = pd.read_csv("still_to_scrap.csv")
+# convert to list
+shortcodes = shortcodes["link"].tolist()
+
+try:
+    # scrap a batch of posts in shortcodes
+    scrap(shortcodes, batch_size=50)
+except:
+    # update the csv not to scrap again the beginning of the batch again
+    shortcodes = pd.DataFrame({"link": shortcodes})
+    shortcodes.to_csv("still_to_scrap.csv")
+
+# convert to dataframe
+shortcodes = pd.DataFrame({"link": shortcodes})
+# save to csv
+shortcodes.to_csv("still_to_scrap.csv")
